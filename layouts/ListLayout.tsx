cf2e200e7b0415b0126d67f09c59156c03a4400f
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-06-09 15:43:11 +0200

    Change blog title

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -63,7 +63,7 @@ export default function ListLayout({
   return (
     <>
       <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
           {title}
         </h1>
         <div className="relative max-w-lg">
